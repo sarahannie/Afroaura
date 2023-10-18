a8c17217368cab75d106728c46413d2592634607
sarahannie <sarahndianekwu@gmail.com>
2023-10-18 17:25:06 +0100

    topad added

--- components/topAd.jsx
+++ components/topAd.jsx
@@ -7,48 +7,46 @@ import Image from 'next/image';
 const TopAd = () => {
   return (
     <>
-        <div className='flex text-white bg-primary py-7 justify-between '>
+        <div className='flex text-white bg-blue py-7 justify-between '>
             <div><FaStar/></div>
-            <div className='text-2xl'> Remaining hours:  <span className='text-3xl'>10h:30m:21s</span></div>
+            <div className='lg:text-2xl text-xl'> Remaining hours:  <span className='lg:text-3xl text-2xl'>10h:30m:21s</span></div>
             <div>SEE ALL-&gt;</div>
         </div>
         <section className="text-gray-600 body-font">
             <div className="container px-18 pt-12 pb-10 mx-auto justify-center">
             <div className="flex flex-wrap justify-center -m-4 gap-12">
-      
-      
-      <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
-   
-   <Image
-    height={18}
-    width={18}
-     className="h-48 w-full object-cover object-end"
-     src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
-     alt="Home in Countryside"
-   />
-   <div className="p-6">
-     <div className="flex items-baseline">
-       <span className="inline-block bg-[#D1E4FC] text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
-         Hot
-       </span>
-       <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-         1 Short • 2 Sock
-       </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
+ 
+ <Image
+ width={30}
+ height={30}
+   className="h-48 w-full object-cover object-end"
+   src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
+   alt="Home in Countryside"
+ />
+ <div className="p-6">
+   <div className="flex items-baseline dark:text-black">
+     <span className="inline-block bg-red text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
+       Hot
+     </span>
+     <div className="ml-2 text-black text-xs uppercase font-semibold tracking-wide">
+       1 Short • 2 Sock
      </div>
-     <h4 className="mt-2 font-semibold text-lg leading-tight truncate">
-       Kids_short
-     </h4>
-     <div className="mt-1 flex justify-between">
-       <div className=" flex flex-col">
-       <span>$1,900.00</span>
-       <span><del>$3,900.00</del></span>
-       </div>
-       <div><span className="ml-2 text-gray-600 text-sm">34 reviews</span></div>
-      
-       
+   </div>
+   <h4 className="mt-2 font-semibold text-black text-lg leading-tight truncate">
+     Kids_short
+   </h4>
+   <div className="mt-1 flex justify-between">
+     <div className=" flex text-black flex-col">
+     <span>$1,900.00</span>
+     <span><del>$3,900.00</del></span>
      </div>
-     <div className="mt-2 flex ">
-       <span className=" flex text-[#020d24] font-semibold ">
+     <div><span className="ml-2 text-black text-sm">34 reviews</span></div>
+    
+     
+   </div>
+   <div className="mt-2 flex text-[#020d24] font-semibold justify-between ">
+       <div className=" flex  ">
          <span className='flex mt-2'>
            <FaStar />
            <FaStar />
@@ -56,43 +54,47 @@ const TopAd = () => {
            <FaStar />
            <FaStar /> 
          </span>
-         <span className="block bg-primary ml-[50px] rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart</span>
-         </span>
+        </div>
+          <div>
+            <span className="block bg-blue   rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart
+            </span>
+          </div> 
+         
+     </div>
+ </div>
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
+ 
+ <Image
+ width={30}
+ height={30}
+   className="h-48 w-full object-cover object-end"
+   src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
+   alt="Home in Countryside"
+ />
+ <div className="p-6">
+   <div className="flex items-baseline dark:text-black">
+     <span className="inline-block bg-red text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
+       Hot
+     </span>
+     <div className="ml-2 text-black text-xs uppercase font-semibold tracking-wide">
+       1 Short • 2 Sock
      </div>
    </div>
+   <h4 className="mt-2 font-semibold text-black text-lg leading-tight truncate">
+     Kids_short
+   </h4>
+   <div className="mt-1 flex justify-between">
+     <div className=" flex text-black flex-col">
+     <span>$1,900.00</span>
+     <span><del>$3,900.00</del></span>
      </div>
-      <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
-   
-   <Image
-    height={18}
-    width={18}
-     className="h-48 w-full object-cover object-end"
-     src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
-     alt="Home in Countryside"
-   />
-   <div className="p-6">
-     <div className="flex items-baseline">
-       <span className="inline-block bg-[#D1E4FC] text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
-         Hot
-       </span>
-       <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-         1 Short • 2 Sock
-       </div>
-     </div>
-     <h4 className="mt-2 font-semibold text-lg leading-tight truncate">
-       Kids_short
-     </h4>
-     <div className="mt-1 flex justify-between">
-       <div className=" flex flex-col">
-       <span>$1,900.00</span>
-       <span><del>$3,900.00</del></span>
-       </div>
-       <div><span className="ml-2 text-gray-600 text-sm">34 reviews</span></div>
-      
-       
-     </div>
-     <div className="mt-2 flex ">
-       <span className=" flex text-[#020d24] font-semibold ">
+     <div><span className="ml-2 text-black text-sm">34 reviews</span></div>
+    
+     
+   </div>
+   <div className="mt-2 flex text-[#020d24] font-semibold justify-between ">
+       <div className=" flex  ">
          <span className='flex mt-2'>
            <FaStar />
            <FaStar />
@@ -100,41 +102,47 @@ const TopAd = () => {
            <FaStar />
            <FaStar /> 
          </span>
-         <span className="block bg-primary ml-[50px] rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart</span>
-         </span>
+        </div>
+          <div>
+            <span className="block bg-blue   rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart
+            </span>
+          </div> 
+         
+     </div>
+ </div>
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
+ 
+ <Image
+ width={30}
+ height={30}
+   className="h-48 w-full object-cover object-end"
+   src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
+   alt="Home in Countryside"
+ />
+ <div className="p-6">
+   <div className="flex items-baseline dark:text-black">
+     <span className="inline-block bg-red text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
+       Hot
+     </span>
+     <div className="ml-2 text-black text-xs uppercase font-semibold tracking-wide">
+       1 Short • 2 Sock
      </div>
    </div>
+   <h4 className="mt-2 font-semibold text-black text-lg leading-tight truncate">
+     Kids_short
+   </h4>
+   <div className="mt-1 flex justify-between">
+     <div className=" flex text-black flex-col">
+     <span>$1,900.00</span>
+     <span><del>$3,900.00</del></span>
      </div>
-      <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
-   
-   <Image
-    height={18}
-    width={18}
-     className="h-48 w-full object-cover object-end"
-     src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
-     alt="Home in Countryside"
-   />
-   <div className="p-6">
-     <div className="flex items-baseline">
-       <span className="inline-block bg-[#D1E4FC] text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
-         Hot
-       </span>
-       <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-         1 Short • 2 Sock
-       </div>
-     </div>
-     <h4 className="mt-2 font-semibold text-lg leading-tight truncate">
-       Kids_short
-     </h4>
-     <div className="mt-1 flex justify-between">
-       <div className=" flex flex-col">
-       <span>$1,900.00</span>
-       <span><del>$3,900.00</del></span>
-       </div>
-       <div><span className="ml-2 text-gray-600 text-sm">34 reviews</span></div>
-     </div>
-     <div className="mt-2 flex ">
-       <span className=" flex text-[#020d24] font-semibold ">
+     <div><span className="ml-2 text-black text-sm">34 reviews</span></div>
+    
+     
+   </div>
+   <div className="mt-2 flex text-[#020d24] font-semibold justify-between ">
+       <div className=" flex  ">
          <span className='flex mt-2'>
            <FaStar />
            <FaStar />
@@ -142,44 +150,47 @@ const TopAd = () => {
            <FaStar />
            <FaStar /> 
          </span>
-         <span className="block bg-primary ml-[50px] rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart</span>
-         </span>
+        </div>
+          <div>
+            <span className="block bg-blue   rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart
+            </span>
+          </div> 
+         
+     </div>
+ </div>
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
+ 
+ <Image
+ width={30}
+ height={30}
+   className="h-48 w-full object-cover object-end"
+   src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
+   alt="Home in Countryside"
+ />
+ <div className="p-6">
+   <div className="flex items-baseline dark:text-black">
+     <span className="inline-block bg-red text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
+       Hot
+     </span>
+     <div className="ml-2 text-black text-xs uppercase font-semibold tracking-wide">
+       1 Short • 2 Sock
      </div>
    </div>
+   <h4 className="mt-2 font-semibold text-black text-lg leading-tight truncate">
+     Kids_short
+   </h4>
+   <div className="mt-1 flex justify-between">
+     <div className=" flex text-black flex-col">
+     <span>$1,900.00</span>
+     <span><del>$3,900.00</del></span>
      </div>
-  
-      <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2">
-   
-   <Image
-    height={18}
-    width={18}
-     className="h-48 w-full object-cover object-end"
-     src="https://images.unsplash.com/photo-1570797197190-8e003a00c846?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
-     alt="Home in Countryside"
-   />
-   <div className="p-6">
-     <div className="flex items-baseline">
-       <span className="inline-block bg-[#D1E4FC] text-black py-1 px-4 text-xs rounded-full uppercase font-semibold tracking-wide">
-         Hot
-       </span>
-       <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-         1 Short • 2 Sock
-       </div>
-     </div>
-     <h4 className="mt-2 font-semibold text-lg leading-tight truncate">
-       Kids_short
-     </h4>
-     <div className="mt-1 flex justify-between">
-       <div className=" flex flex-col">
-       <span>$1,900.00</span>
-       <span><del>$3,900.00</del></span>
-       </div>
-       <div><span className="ml-2 text-gray-600 text-sm">34 reviews</span></div>
-      
-       
-     </div>
-     <div className="mt-2 flex ">
-       <span className=" flex text-[#020d24] font-semibold ">
+     <div><span className="ml-2 text-black text-sm">34 reviews</span></div>
+    
+     
+   </div>
+   <div className="mt-2 flex text-[#020d24] font-semibold justify-between ">
+       <div className=" flex  ">
          <span className='flex mt-2'>
            <FaStar />
            <FaStar />
@@ -187,12 +198,16 @@ const TopAd = () => {
            <FaStar />
            <FaStar /> 
          </span>
-         <span className="block bg-primary ml-[50px] rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart</span>
-         </span>
+        </div>
+          <div>
+            <span className="block bg-blue   rounded-full text-white text-xs font-bold px-3 py-2 leading-none flex items-center">Add Cart
+            </span>
+          </div> 
+         
      </div>
-   </div>
-     </div>
-      </div>
+ </div>
+            </div>
+            </div>
             </div>
         </section>
     </>
