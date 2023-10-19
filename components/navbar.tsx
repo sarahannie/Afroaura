9f339d529627e6cb40e3304d54f4fa430811128d
sarahannie <sarahndianekwu@gmail.com>
2023-10-19 21:25:55 +0100

    adjusted navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -26,15 +26,15 @@ export const Navbar = () => {
       <div className={style.nav}>
         <Image src="/img/logo.png" alt="Logo" width={40} height={40} />FROAURA
       </div>
-      <div className="hidden lg:block"> {/* Hide on small screens */}
-        <form onSubmit={handleSubmit}>
+      <div className="hidden lg:block "> {/* Hide on small screens */}
+        <form onSubmit={handleSubmit} className='flex gap-2'>
           <label htmlFor="chat-input" className="sr-only">Search...</label>
           <div className="relative">
             <input
               id="search-chats"
               type="text"
-              className="w-full rounded-lg border border-blue bg-slate-50 p-3 pr-40 text-sm text-slate-800 focus:outline-none focus:ring-2 focus:ring-blue dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200"
-              placeholder="Search..."
+              className="w-full rounded-lg border border-blue bg-slate-50 p-3 pr-80 text-sm text-slate-800 focus:outline-none focus:ring-2 focus:ring-blue dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200"
+              placeholder="Search Products..."
               required
               value={searchTerm}
               onChange={handleInputChange}
@@ -43,19 +43,17 @@ export const Navbar = () => {
               type="submit"
               className="absolute bottom-2 right-2.5 rounded-lg p-2 text-sm text-slate-500 hover:text-blue-700 focus:outline-none sm:text-base"
             >
-              <FcSearch className="h-5 w-5" />
+              
               <span className="sr-only">Search...</span>
             </button>
           </div>
+          <Button className='bg-blue text-white mt-1 hover:bg-violent' radius="none" variant="solid" >SEARCH </Button>
         </form>
       </div>
       <div className={style.btn}>
         <ThemeSwitch />
-        <Button className='bg-[#7f0577] text-white' radius="none" variant="solid">
-          Signup
-        </Button>
         <Button  radius="none" variant="bordered" className='hidden lg:block border-[#7f0577] text-[#7f0577]'>
-          Join Us
+          Signup
         </Button>
       </div>
     </div>
